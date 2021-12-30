c6d57b299b295d642eaf04fc1613758725b89d7a
Changoh Yoo <yooco0618@gmail.com>
2021-12-30 20:54:56 +0900

    chore: CI 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">이민교</router-link> |
+    <router-link to="/about">유창오</router-link>
   </div>
   <router-view />
 </template>
