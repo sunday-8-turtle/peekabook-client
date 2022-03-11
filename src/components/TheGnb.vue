c6491c10aea994f4406bdd70aa59582d67b7e78a
Changoh Yoo <yooco0618@gmail.com>
2022-03-12 01:47:34 +0900

    feat: Landing Page 기본 레이아웃

--- src/components/TheGnb.vue
+++ src/components/TheGnb.vue
@@ -167,6 +167,11 @@ export default defineComponent({
 <style lang="scss" scoped>
 header {
   height: 72px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
 
   nav {
     height: 100%;
