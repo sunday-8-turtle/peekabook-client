e4202c27538be115a52638002245c8c41d8fcf00
Changoh Yoo <yooco0618@gmail.com>
2022-03-20 22:01:43 +0900

    fix: PWA, Workbox 제거

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // pwa: {
+  //   name: 'Peekabook',
+  //   workboxOptions: {
+  //     exclude: ['_redirects'],
+  //   },
+  // },
+  chainWebpack: (config) => {
+    config.plugins.delete('pwa');
+    config.plugins.delete('workbox');
+  },
+};
