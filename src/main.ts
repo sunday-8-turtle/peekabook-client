b7b9b6b3eddc34b9c94596c4155aad2de833ecda
eric <eric@hphk.kr>
2022-03-20 20:31:06 +0900

    fix: remove serviceWorker

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
-import './registerServiceWorker';
 import router from './router';
 
 createApp(App).use(createPinia()).use(router).mount('#app');
